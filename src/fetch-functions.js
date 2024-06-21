206a97dfbbf799aa919feda7ed035f9da71e868c
embryobirthday <alexinverda@gmail.com>
2024-06-21 11:02:42 -0400

    progress

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,9 +1,26 @@
 const userUrl = 'https://jsonplaceholder.typicode.com/users'
 
 export const checkResponseStatus = () => {
+    //defining the variable that holds the fetch promise, chaining the then
+    const checkStatus = fetch(userUrl).then((response) => {
+        //returning the data from the response promise in an object using object keys 
+        return { "ok": response.ok, "status": response.status, "url": response.url };
+
+    });
+    //returning the promise itself and the data it has with it
+    return checkStatus
 };
 
 export const getUsers = () => {
+    const userDataFetch = fetch(userUrl).then((response) => {
+
+        return {
+            id: response.
+        }
+
+    })
+
+    return userDataFetch
 };
 
 export const getUserPosts = () => {
